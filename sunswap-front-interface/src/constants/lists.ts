552555b874f4abca94339ef5a9f895f95c17c0b6
congson1907 <congson1907vn@gmail.com>
2023-07-05 01:12:33 +0700

    add

--- sunswap-front-interface/src/constants/lists.ts
+++ sunswap-front-interface/src/constants/lists.ts
@@ -1,4 +1,6 @@
 // the Sunswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = process.env.DEFAULT_TOKEN_LIST_URL ?? 'ipfs://QmVhFYqWVKmzVt9MuQPhghou6XZsgMNma4EENcfJFpJTqp'
+import dotenv from 'dotenv';
+dotenv.config();
+export const DEFAULT_TOKEN_LIST_URL = process.env.DEFAULT_TOKEN_LIST_URL ?? 'ipfs://QmdSTgiyAwD5hWLDq6tTyea7ke27Hz1bPfRQq3eNseoq3D'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL]
